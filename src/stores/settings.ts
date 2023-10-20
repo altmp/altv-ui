4455170ab5f32737235f37b83d36a0903d666b66
Artem Dzhemesiuk <programming@zziger.me>
2023-10-20 13:28:49 +0200

    Fixed objects in settings

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -2,7 +2,7 @@ import {defineStore} from "pinia";
 import {useInitializableStore} from "@/stores/storeInitializer";
 import {useUIStore} from "@/stores/ui";
 import {ModalType, useModalStore} from "@/stores/modal";
-import {watch} from "vue";
+import {toValue, watch} from "vue";
 
 export const useSettingsStore = useInitializableStore(defineStore('settings', {
     state: () => {
@@ -84,7 +84,7 @@ export const useSettingsStore = useInitializableStore(defineStore('settings', {
             })
         },
         save(key: keyof typeof this.$state['data']) {
-            alt.emit('settings:change', key, this.data[key]);
+            alt.emit('settings:change', key, toValue(this.data[key]));
         }
     },
 
